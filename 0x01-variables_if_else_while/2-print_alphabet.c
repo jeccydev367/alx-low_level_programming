bd6e01b5d14fa3cc16f451ddef7aae42e6eab13e
jeccydev367 <janechinyere635@gmail.com>
2022-09-11 12:03:48 -0700

    g

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,28 +1,28 @@
-#include <stdio.h> 
-  
- /** 
-  * main - prints all possible different combinations of two digits 
-  * Return: ALways 0 (Success) 
-  */ 
- int main(void) 
- { 
-         int i, j; 
-  
-         for (i = 48; i <= 56; i++) 
-         { 
-                 for (j = 49; j <= 57; j++) 
-                 { 
-                         if (i < j) 
-                         { 
-                                 putchar(i); 
-                                 putchar(j); 
-                                 if (i == 56 && j == 57) 
-                                         continue; 
-                                 putchar(','); 
-                                 putchar(' '); 
-                         } 
-                 } 
-         } 
-         putchar('\n'); 
-         return (0); 
+#include <stdio.h>
+
+ /**
+  * main - prints all possible different combinations of two digits
+  * Return: ALways 0 (Success)
+  */
+ int main(void)
+ {
+         int i, j;
+
+         for (i = 48; i <= 56; i++)
+         {
+                 for (j = 49; j <= 57; j++)
+                 {
+                         if (i < j)
+                         {
+                                 putchar(i);
+                                 putchar(j);
+                                 if (i == 56 && j == 57)
+                                         continue;
+                                 putchar(',');
+                                 putchar(' ');
+                         }
+                 }
+         }
+         putchar('\n');
+         return (0);
  }
