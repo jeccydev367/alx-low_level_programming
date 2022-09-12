7ec945d194ea921b31f0b047db0f2f15ebc369b6
jeccydev367 <janechinyere635@gmail.com>
2022-09-12 07:55:47 -0700

    g

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ *main - prints numbers between 00 to 89.
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 48; i <= 56; i++)
+{
+for (j = 49; j <= 57; j++)
+{
+if (i < j)
+{
+putchar(i);
+putchar(j);
+if (i == 56 && j == 57)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
