1135e66d664d49d771354c7973988e471ab1a2ab
jeccydev367 <janechinyere635@gmail.com>
2022-09-11 18:26:54 -0700

    d

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ *main - prints decimal digits
+ *followed by a new line
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+while (n = 0, n < 10, n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
