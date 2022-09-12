5e1f53c8459acfaa9c83a322f6c054f4765e94c6
jeccydev367 <janechinyere635@gmail.com>
2022-09-11 19:10:09 -0700

    git

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ *main - print reversed alphabet.
+ *
+ *Return
+ */
+int main(void)
+{
+int i;
+for (i = 122; i > 96; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
