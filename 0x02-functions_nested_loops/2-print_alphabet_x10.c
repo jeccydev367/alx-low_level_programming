0baf12f2c2b2665ea943b19842a9b7f4a5e9e217
jeccydev367 <janechinyere635@gmail.com>
2022-09-13 16:33:40 -0700

    g

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,20 @@
-#include"main.h" 
-  
- /** 
-  * add - adds two integers 
-  * @n1: integer 1 
-  * @n2: integer 2 
-  * 
-  * Description: give two integers, returns sum 
-  * 
-  * Return: sum 
-  */ 
-  
- int add(int n1, int n2) 
- { 
-         int sum; 
-  
-         sum = n1 + n2; 
-  
-         return (sum); 
- }
+#include "main.h"
+
+/**
+ * add - adds two intergers
+ * @n1: interger 1
+ * @n2: interger 2
+ *
+ * Description: give two intergers, returns sum
+ *
+ * Return: sum
+ */
+
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+
+	return (sum);
+}
