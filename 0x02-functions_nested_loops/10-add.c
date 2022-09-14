55b4664aafdb4765cbffa489f339a4f0630dbcbc
jeccydev367 <janechinyere635@gmail.com>
2022-09-13 18:56:28 -0700

    x

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -7,7 +7,7 @@
  *
  * Description: give two integers, returns sum
  *
- * Retyrn: sum
+ * Return: sum
  */
 
 int add(int n1, int n2)
@@ -15,6 +15,6 @@ int add(int n1, int n2)
 	int sum;
 
 	sum = n1 + n2;
-	
+
 	return (sum);
 }
