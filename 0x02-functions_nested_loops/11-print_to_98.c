efe5203e82fb9ff3cf72cba0136d81aa65f2f64f
jeccydev367 <janechinyere635@gmail.com>
2022-09-13 19:24:17 -0700

    x

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while (n > 98)
+		while (n < 98)
 		{
 			printf("%d,", n);
 			n++;
@@ -26,7 +26,7 @@ void print_to_98(int n)
 		{
 			printf("%d,", n);
 		n--;
-	        
+
 	        }
         }
 printf("98\n");
