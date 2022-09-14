31a76b9de7ff97f6b37aaf574daf5a9faa7112b4
jeccydev367 <janechinyere635@gmail.com>
2022-09-13 20:31:09 -0700

    g

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,51 @@
-#include"main.h"
-/**
-* print_times_table - prints the times table
-* @n: integer for which the times table will be printed
-*
-* Description: prints the times table
-*
-* Return: void
-*/
-void print_times_table(int n)
-{
-       int row, column, product;
+#include "main.h"
 
-       if (n >= 0 && n < 15)
-       {
-               for (row = 0; row <= n; row++)
-               	{
-                       for (column = 0; column <= n; column++)
-                       {
-                               product = row * column;
+ /**
+  * print_times_table - Prints a multiplication table up to param
+  * @n: The number to be treated
+  *
+  * Return: Number matrix
+  */
+ void print_times_table(int n)
+ {
+         int x, y, z;
 
-                               if (column == 0)
-                                       	_putchar('0');
-                                 else if (product < 10)
+         if (n >= 0 && n <= 14)
+         {
+                 for (x = 0; x <= n; x++)
+                 {
+                         for (y = 0; y <= n; y++)
+                         {
+                                 z = x * y;
+                                 if (z > 99)
                                  {
                                          _putchar(',');
-                                         _putchar(' ');
-                                         _putchar(' ');
-                                         _putchar(' ');
-                                         _putchar(product % 10 + '0');
+                                         _putchar(32);
+                                         _putchar((z / 100) + '0');
+                                         _putchar(((z / 10) % 10) + '0');
+                                         _putchar((z % 10) + '0');
                                  }
-                                 else if (product >= 10 && product < 100)
+                                 else if (z > 9)
                                  {
                                          _putchar(',');
-                                         _putchar(' ');
-                                         _putchar(' ');
-                                         _putchar((product / 10) % 10 + '0');
-                                         _putchar(product % 10 + '0');
+                                         _putchar(32);
+                                         _putchar(32);
+                                         _putchar(((z / 10) % 10) + '0');
+                                         _putchar((z % 10) + '0');
                                  }
-                                 else if (product > 99 && product < 1000)
+                                 else
                                  {
-                                         _putchar(',');
-                                         _putchar(' ');
-                                         _putchar(product / 100 + '0');
-                                         _putchar((product / 10) % 10 + '0');
-                                         _putchar(product % 10 + '0');
+                                         if (y != 0)
+                                         {
+                                                 _putchar(',');
+                                                 _putchar(32);
+                                                 _putchar(32);
+                                                 _putchar(32);
+                                         }
+                                         _putchar(z + '0');
                                  }
                          }
                          _putchar('\n');
                  }
-        }
+         }
  }
