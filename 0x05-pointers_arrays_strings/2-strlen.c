df20c7af51a0810ff4dd5b8b1033078ef342d732
jeccydev367 <janechinyere635@gmail.com>
2022-09-19 04:39:52 -0700

    c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - Return the qty f bytes that a string has
+ * @s: The string
+ *
+ * Return: The long of the string as an integer number
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
